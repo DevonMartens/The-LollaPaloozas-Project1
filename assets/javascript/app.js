dda0d36cabc0839090d74f1245d5a9942ebc4ae2
Devon Martens <dmmartens91@gmail.com>
2019-10-05 13:00:06 -0400

    java psedo code started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,56 @@
 /*section 3 id="section3" */
 /*section 3 id="tourDatesImage" */
 /*section 3 header id="tourDates" */
+
+
+
+
+//on click calling button #findBand//
+
+//section 1 2 3 empy//
+//empty carosol logo//
+
+//section 1 summary hit wikipedia api//
+//ajax call//
+
+//how example//
+
+//hit headers, phots, text for each section//
+//see empty div code from trivia game//
+$(document).on("click", ".topics", function () {
+
+    $('#gifArea').empty();
+
+    $('#addTopic').removeClass("active");
+
+    $(this).addClass('active');
+
+
+
+    var type = $(this).attr("data-type");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=gAqN16PRJ6aBi1cl5I5ZqVkcFB8zp3WG";
+
+   console.log(queryURL);
+
+   $.ajax ({
+
+       url: queryURL,
+
+       method: "GET"
+
+   })
+
+
+
+.then(function(response) {
+
+    var results = response.data;
+
+   console.log(results);
+
+   for (var i = 0; i < results.length; i++) { 
+
+       var animalDiv = $("<div class=\"animal-item\">");
+
+   
