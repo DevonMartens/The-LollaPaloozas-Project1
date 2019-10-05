80b27e8b657a48bd4a809b19e0feecc6115ee0ab
Devon Martens <dmmartens91@gmail.com>
2019-10-05 11:12:58 -0400

    id's and format

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+/*carosol is band pictures*/
+/*section 1 id is band summary*/
+/*"section1" id is for card*/
+/*"section1" id is for card*/
+/* section 1 one headline id is id="summary">
+/*section 2 id is top hits*/
+/*section 2 id="section2"*/
+/*header id is topHits*/
+/*Section 2 image id topHitsImage*/
+/*section 3  is tour cities tour dates*/
+/*section 3 id="section3" */
+/*section 3 id="tourDatesImage" */
+/*section 3 header id="tourDates" */
